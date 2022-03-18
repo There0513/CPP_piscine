d16dde6a067ba0c01c35ead182b103df9657a455
Theresa <theresa.reiss@epitech.eu>
2022-03-18 09:56:29 +0100

    mod04 ex03 almost done

--- CPP_module04/ex03/MateriaSource.cpp
+++ CPP_module04/ex03/MateriaSource.cpp
@@ -1,27 +1,50 @@
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource( void ) {
+MateriaSource::MateriaSource( void ): _inventory() {
     std::cout << "Constructor MateriaSource" << std::endl;
 }
 
-MateriaSource::MateriaSource( MateriaSource const & copy ) {
-
+MateriaSource::MateriaSource( MateriaSource const & copy ): _inventory() {
+    for (int i = 0; i < 4; i++)
+        if (_inventory[i])
+            delete _inventory[i];
+    for (int i = 0; i < 4; i++)
+        if (copy._inventory[i])
+            this->_inventory[i] = copy._inventory[i]->clone();
 }
 
 MateriaSource & MateriaSource::operator=( MateriaSource const & copy) {
-
+    for (int i = 0; i < 4; i++)
+        delete _inventory[i];
+    for (int i = 0; i < 4; i++)
+        this->_inventory[i] = copy._inventory[i]->clone();
+    return *this;
 }
 
 MateriaSource::~MateriaSource( void ) {
     std::cout << "Destructor MateriaSource" << std::endl;
+    for (int i = 0; i < 4; i++)
+        if (_inventory[i])
+            delete this->_inventory[i];
 }
 
 
     /* from IMateriaSource Interface: */
-void        MateriaSource::learnMateria( AMateria* ) {
-
+void        MateriaSource::learnMateria( AMateria* tocopy) {
+// Copies the Materia passed as a parameter and store it in memory so it can be cloned
+// later. Like the Character, the MateriaSource can know at most 4 Materias. They
+// are not necessarily unique.
+    for (int i = 0; i < 4; i++) {
+        if (!_inventory[i])
+            _inventory[i] = tocopy;
+            return ;
+    }
+    // if (i == 4)
+    //     delete tocopy;
 }
 
 AMateria*   MateriaSource::createMateria( std::string const & type ) {
-
+// Returns a new Materia. The latter is a copy of the Materia previously learned by
+// the MateriaSource whose type equals the one passed as parameter. Returns 0 if
+// the type is unknown.
 }
