85b83f76003ea12e8de124dd5841af2a36bf2f56
Theresa REISS <threiss@e3r2p11.clusters.42paris.fr>
2022-04-12 17:28:21 +0200

    mod08 ex00 OK

--- CPP_module08/ex00/easyfind.hpp
+++ CPP_module08/ex00/easyfind.hpp
@@ -1,11 +1,16 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 # include <algorithm>
+# include <vector>
+# include <iostream>
+#include <cstdlib>     /* srand, rand */
 
 template< typename T>
 typename T::iterator    easyfind(T &container, int nbr) {
-
-// https://www.cplusplus.com/reference/algorithm/find/
+    typename T::iterator it = find(container.begin(), container.end(), nbr);
+    if (it == container.end())
+        throw std::runtime_error("Not found or empty.");
+    return it;
 }
 
 #endif
---
+++ CPP_module08/ex00/main.cpp
@@ -0,0 +1,48 @@
+#include "easyfind.hpp"
+
+int main( void )
+{
+    srand(time(NULL));
+    std::cout << "\t\033[1;33mempty vector:\033[0m" << std::endl;
+    std::vector<int> vec;
+    try
+    {
+        std::vector<int>::iterator it = easyfind(vec, 0);
+        std::cout << "it = " << *it << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    std::cout << "\t\033[1;33mVec 0 - 50:\033[0m" << std::endl;
+    for (int i = 0; i < 50; i++)
+        vec.push_back(i);
+    for (int i = 0; i < 50; i++)
+        std::cout << "vec 1 = " << vec[i] << std::endl;
+    try
+    {
+        std::vector<int>::iterator it = easyfind(vec, 42);
+        std::cout << "it = " << *it << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    std::cout << "\t\033[1;33mVec2 random:\033[0m" << std::endl;
+    std::vector<int> vec2;
+
+    for (int i = 0; i < 5; i++)
+        vec2.push_back(rand());
+    for (int i = 0; i < 5; i++)
+        std::cout << "vec2 = " << vec2[i] << std::endl;
+    try
+    {
+        std::vector<int>::iterator it = easyfind(vec2, 52165);
+        std::cout << "it = " << *it << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    return 0;
+}
