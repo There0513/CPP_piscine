ca7d57052a81b471d59c254f4012719f5aecefa7
Theresa <theresa.reiss@epitech.eu>
2022-04-12 13:04:45 +0200

    mod08 ex00 starter

---
+++ CPP_module08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# include <algorithm>
+
+template< typename T>
+typename T::iterator    easyfind(T &container, int nbr) {
+
+// https://www.cplusplus.com/reference/algorithm/find/
+}
+
+#endif
